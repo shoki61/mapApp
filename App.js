81101cf60cfd0c0921563cafd622569ff0862ab1
Sohrat <sohrat6128@gmail.com>
2020-01-19 12:25:59 +0300

    version_1

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View as Div,Text as P} from "react-native";
+
+
+class Map extends React.Component{
+  render(){
+      return(
+        <Div>
+          <P>SALAMLAR</P>
+        </Div>
+    );
+  }
+}
+
+export default Map;
