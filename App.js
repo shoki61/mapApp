b42876f7bb0c36cec4863d33cad5fef7b76e2941
Sohrat <sohrat6128@gmail.com>
2020-01-19 14:35:26 +0300

    version_1.01

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
-import React from "react";
-import {View as Div,Text as P} from "react-native";
+import React from 'react';
+import {View,Text} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import still from './style'
+
 
 
 class Map extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {mapType:'hybrid'}
+  }
   render(){
       return(
-        <Div>
-          <P>SALAMLAR</P>
-        </Div>
+        <View style={still.container}>
+          <MapView
+            region={{
+              latitude:27,
+              longitude:27,
+              latitudeDelta:30,
+              longitudeDelta:30
+            }}
+            mapType={this.state.mapType}
+          >
+            <Marker
+              coordinate={{
+                latitude:27,
+                longitude:27
+              }}
+              title={'Buradayım'}
+              description={'Burası bir bölge'}
+            />
+          </MapView>
+        </View>
     );
   }
 }
