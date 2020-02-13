0687c1f7fb7b0febdcbee42d4334e6168cd84644
Sohrat <sohrat6128@gmail.com>
2020-02-13 23:33:30 +0300

    1.04

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View,TextInput,Image,TouchableOpacity,Text,PermissionsAndroid} from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
 import styles from './style'
 
 
@@ -11,11 +13,15 @@ class Map extends React.Component{
     this.state = {
       mapType:'standard',
       trafficInfo: false,
+      mode:'WALKING',
       region: {
-        latitude: 56,
-        longitude: 56,
-        latitudeDelta:56,
-        longitudeDelta: 56,
+        latitude: 40.983879,
+        longitude: 37.876411,
+      },
+      region1: {
+        latitude: 41.00145,
+        longitude:39.7178,
+        
       }
     }
   }
@@ -39,16 +45,28 @@ class Map extends React.Component{
             style={styles.mapContainer}
             mapType={this.state.mapType}
             showsMyLocationButton
-            followsUserLocation
-            showsUserLocation
+			showsUserLocation
+			zoomEnabled
+			zoomControlEnabled
             showsTraffic={this.state.trafficInfo}
-            zoomControlEnabled
-            provider={PROVIDER_GOOGLE}
           >
+            <MapViewDirections
+              origin={this.state.region}
+              destination={this.state.region1}
+              apikey={'AIzaSyCs_G0OGN30lNt-E_K6BzVjEqznCGZx8bM'}
+              strokeWidth={4}
+			  strokeColor={'red'}
+		      mode={this.state.mode}
+            />
             <Marker
+              title={'Başlık'}
+              description={'Bu bir açıklama'}
               coordinate={ this.state.region }
               draggable
+              icon={require('./images/pinIcon.png')}
             />
+            
+            
           </MapView>
 
           <View style={styles.inputContainer}>
@@ -62,11 +80,11 @@ class Map extends React.Component{
               }
             </TouchableOpacity>
           </View>
+            
          
           
-          
 
-          <TouchableOpacity onPress={()=>this.changeMapType()} style={styles.mapTypeButton}>
+          <TouchableOpacity  onPress={()=>this.changeMapType()} style={styles.mapTypeButton}>
             {this.state.mapType === 'standard' && <Image source={require('./images/hybrid.png')} style={styles.mapTypeIcon}/>}
             {this.state.mapType === 'hybrid' && <Image source={require('./images/satellite.png')} style={styles.mapTypeIcon}/>}
             <Text style={styles.mapTypeText}>click to change</Text>
